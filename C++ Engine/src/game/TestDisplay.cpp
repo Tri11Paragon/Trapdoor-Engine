734b6ffcb28ab4ba06667d447289426a23fee4d9
Brett <brettmaster1@gmail.com>
2022-07-31 16:18:44 -0400

    Added color formatting

---
+++ C++ Engine/src/game/TestDisplay.cpp
@@ -0,0 +1,8 @@
+//
+// Created by brett on 31/07/22.
+//
+
+#include "TestDisplay.h"
+
+namespace TD {
+} // TD
---
+++ C++ Engine/src/game/TestDisplay.h
@@ -0,0 +1,16 @@
+//
+// Created by brett on 31/07/22.
+//
+
+#ifndef ENGINE_TESTDISPLAY_H
+#define ENGINE_TESTDISPLAY_H
+
+namespace TD {
+
+    class TestDisplay {
+
+    };
+
+} // TD
+
+#endif //ENGINE_TESTDISPLAY_H
